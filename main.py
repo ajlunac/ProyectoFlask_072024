d1320bae643f3971a81c91c962bb520d6ec71a01
Javier Luna Castellon <lunaion2200@gmail.com>
2024-07-29 18:41:18 -0500

    Rutas con flask

--- main.py
+++ main.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return "Hola desde tu servidor Flask"
 
+@app.route('/acercade')
+def acercade():
+    return "<h1>Acerca de mi<h1>"
+
+# Ruta con parametros.
+@app.route('/saludame') 
+@app.route('/saludame/<string:nombre>') 
+@app.route('/saludame/<string:nombre>/<int:edad>')
+def saliudame(nombre='Javier', edad=None):
+    if edad != None:
+        return "Hola {} tienes {} años".format(nombre, edad)
+    else:
+            return f"""
+                    <h1>Hola, </h1>
+                    <h3>{nombre} </h3>
+                    """
+@app.route('/suma/<int:numero1>/<int:numero2>')
+def suma(numero1, numero2):
+    suma = numero1 + numero2
+    return f"La suma es igual a {suma}"
+
 if __name__ == '__main__':
     app.run(debug=True)
