89936f3a6f008f2e2c05e90e027cb65e53627b16
Javier Luna Castellon <lunaion2200@gmail.com>
2024-07-29 19:00:09 -0500

    Redirección de rutas

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola desde tu servidor Flask"
+    return "Index o página principal"
 
+@app.route('/redirecciona')
+@app.route('/redirecciona/<string:sitio>')
+def redirecciona(sitio=None):
+    if sitio is not None:
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('acercade'))
+    
 @app.route('/acercade')
 def acercade():
     return "<h1>Acerca de mi<h1>"
@@ -18,10 +26,10 @@ def saliudame(nombre='Javier', edad=None):
     if edad != None:
         return "Hola {} tienes {} años".format(nombre, edad)
     else:
-            return f"""
-                    <h1>Hola, </h1>
-                    <h3>{nombre} </h3>
-                    """
+        return f"""
+                <h1>Hola, </h1>
+                <h3>{nombre} </h3>
+                """
 @app.route('/suma/<int:numero1>/<int:numero2>')
 def suma(numero1, numero2):
     suma = numero1 + numero2
